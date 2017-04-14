72638f1c6ef8db64b6bf7e2563e8897a1bba72c4
mattRCB <mattsteinrcb@gmail.com>
2017-04-14 17:48:30 -0400

    mongoose

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,7 @@
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render('index', {
-			placeholder: "whatever",
-			layout: false
+			placeholder: "whatever"
 		})
 	})
 
--- server.js
+++ server.js
@@ -4,10 +4,33 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var cheerio = require('cheerio')
 var exphbs = require('express-handlebars')
-var logger = require("morgan")
+var logger = require('morgan')
+var mongoose = require('mongoose')
 
 var app = express()
 var port = process.env.PORT || 8080
+
+
+// // Database configuration
+// var databaseUrl = process.env.MONGODB_URI || "newscrape_db";
+// var collections = ["articles"];
+// // Hook mongojs config to db variable
+// var db = mongojs(databaseUrl , collections);
+// // Log any mongojs errors to console
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
+
+
+
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/newscrape_db');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`Mongoose connected to database.`)
+});
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -15,7 +38,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 // setup express-hanblebars
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
+app.engine('hbs', exphbs({ extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
 
